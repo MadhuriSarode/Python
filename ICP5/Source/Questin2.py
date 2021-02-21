2aa8c8a2c9088a65c36b0d5f887e1ac5342f073c
Madhuri Sarode <54294671+MadhuriSarode@users.noreply.github.com>
2021-02-21 00:29:11 -0600

    ICP5

--- ICP5/Source/Questin2.py
+++ ICP5/Source/Questin2.py
@@ -40,3 +40,29 @@ kot = corr[corr >= .9]
 plt.figure(figsize=(12, 8))
 sns.heatmap(kot, cmap="Greens")
 plt.show()
+
+
+# Question 3 : Building the model only with the correlated features
+# Reading the data from the CSV file
+restaurant_data1 = pandas.read_csv('Restaurant_Revenue_Predictiondataset.csv')
+y = np.log(restaurant_data1.revenue)  # Build a linear model, drop the columns which are not numeric
+X = restaurant_data1.drop(
+    ['revenue', 'City Group', 'Type', 'P1', 'P3', 'P4', 'P5', 'P7', 'P8', 'P9', 'P10', 'P11', 'P12', 'P14', 'P15',
+     'P16', 'P17', 'P18', 'P19', 'P20', 'P21', 'P22', 'P23', 'P24', 'P25', 'P26', 'P27', 'P30', 'P31', 'P32', 'P33',
+     'P34', 'P35', 'P36', 'P37'], axis=1)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, random_state=50, test_size=0.13)  # Split the data into test and training sets
+
+# Training the multiple regression model based on training dataset
+lr1 = linear_model.LinearRegression()
+model1 = lr1.fit(X_train, y_train)
+
+# Evaluate the performance using test dataset and visualize results
+print("R^2 using only top 5 correlated features is: \n", model1.score(X_test, y_test))
+predictions = model1.predict(X_test)
+print('RMSE using only top 5 correlated features is: \n', mean_squared_error(y_test, predictions))
+
+# show the plot of data and the linear regression model fit
+sns.regplot(x=y_test, y=predictions, ci=None, color="b")
+plt.show()
+
